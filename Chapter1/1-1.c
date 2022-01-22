330d8ef22ccdb3994075c0c3db8dedbbcd4f0bc2
Otoss <97791320+Otoss@users.noreply.github.com>
2022-01-22 11:30:48 +0800

    Add files via upload

---
+++ Chapter1/1-1.c
@@ -0,0 +1,12 @@
+/*
+ * @Author: Wink
+ * @Date: 2022-01-22 11:17:49
+ * @LastEditTime: 2022-01-22 11:21:46
+ * @Description: A Hello World Program.
+ * Copyright (c) 2022 by Wink, All Rights Reserved. 
+ */
+#include <stdio.h>
+int main() {
+    printf("Hello,World!");
+    return 0;
+}
