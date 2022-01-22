c2c911c51d810f1a48e807efba3a03920f7ed01e
Otoss <97791320+Otoss@users.noreply.github.com>
2022-01-22 11:36:37 +0800

    Add files via upload

---
+++ Chapter1/1-2.c
@@ -0,0 +1,12 @@
+/*
+ * @Author: Wink
+ * @Date: 2022-01-22 11:32:22
+ * @LastEditTime: 2022-01-22 11:35:50
+ * @Description: 
+ * Copyright (c) 2022 by Wink, All Rights Reserved. 
+ */
+#include <stdio.h>
+int main() {
+    printf("Hello,\nC!\c");
+    return 0;
+}
